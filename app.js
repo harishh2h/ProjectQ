92a2926ce849f58b7d824fecd763b500b3d7babe
Harish <h2hharish6@gmail.com>
2022-06-22 16:54:03 +0530

    deploying

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { time } = require("console");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname));
